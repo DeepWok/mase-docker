13bd25eb0c9ecaf72d2d4d78f4f67fe653fee2ac
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2024-04-24 12:50:57 +0000

    Downgrade numpy for now because python3.11 does not support 2.0

--- install-pips.sh
+++ install-pips.sh
@@ -17,5 +17,6 @@ pip3 install onnx black toml GitPython colorlog cocotb[bus]==1.8.0 \
                  ghp-import optimum pytest-profiling myst_parser \
                  pytest-cov pytest-xdist pytest-sugar pytest-html \
                  lightning wandb bitarray bitstring emoji \
+                 "numpy<2.0" \
     && pip install -U Pillow \
     && pip install mpmath==1.3.0 
