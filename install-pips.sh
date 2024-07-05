9cc0e3453527c14dc725dddcedfcbe182926872c
Aaron Zhao <Aaron-Zhao123@users.noreply.github.com>
2024-07-05 23:28:34 +0100

    Dev/more pip install (#29)
    * more sphinx stuff
    
    * more missing packages in pip install

--- install-pips.sh
+++ install-pips.sh
@@ -13,11 +13,16 @@ pip3 install onnx black toml GitPython colorlog cocotb[bus]==1.8.0 \
                  tabulate tensorboardx hyperopt accelerate \
                  optuna stable-baselines3[extra] h5py scikit-learn \
                  scipy onnxruntime matplotlib sphinx-rtd-theme \
+                 sphinx-test-reports sphinxcontrib-plantuml \
+                 sphinx-needs \
                  imageio imageio-ffmpeg opencv-python kornia einops \
                  ghp-import optimum pytest-profiling myst_parser \
                  pytest-cov pytest-xdist pytest-sugar pytest-html \
                  lightning wandb bitarray bitstring emoji \
                  "numpy<2.0" tensorboard sphinx_needs \
                  onnxconverter-common absl-py sphinx-glpi-theme prettytable \
+                 pyyaml pynvml "bitstring>=4.2" \
+                 myst_parser \
+                 cvxpy \
     && pip install -U Pillow \
     && pip install mpmath==1.3.0
