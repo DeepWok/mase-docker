5e7b47d77575d64ba218463a9fb8212bf4d8414a
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2024-07-10 11:19:38 +0100

    Changed to install packages from mase sources (#30)
    * Changed to install packages from mase sources
    
    * Changed git clone protocol

--- install-pips.sh
+++ install-pips.sh
@@ -6,23 +6,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-pip3 install onnx black toml GitPython colorlog cocotb[bus]==1.8.0 \
-                 pytest pytorch-lightning transformers toml \
-                 timm pytorch-nlp datasets ipython ipdb \
-                 sentencepiece einops deepspeed pybind11 \
-                 tabulate tensorboardx hyperopt accelerate \
-                 optuna stable-baselines3[extra] h5py scikit-learn \
-                 scipy onnxruntime matplotlib sphinx-rtd-theme \
-                 sphinx-test-reports sphinxcontrib-plantuml \
-                 sphinx-needs \
-                 imageio imageio-ffmpeg opencv-python kornia einops \
-                 ghp-import optimum pytest-profiling myst_parser \
-                 pytest-cov pytest-xdist pytest-sugar pytest-html \
-                 lightning wandb bitarray bitstring emoji \
-                 "numpy<2.0" tensorboard sphinx_needs \
-                 onnxconverter-common absl-py sphinx-glpi-theme prettytable \
-                 pyyaml pynvml "bitstring>=4.2" \
-                 myst_parser \
-                 cvxpy \
-    && pip install -U Pillow \
-    && pip install mpmath==1.3.0
+# Install MASE pip dependencies
+mkdir -p /srcPkgs \
+    && cd /srcPkgs \
+    && git clone https://github.com/DeepWok/mase.git \
+    && cd mase \
+    && pip3 install -e . \
+    && cd .. \
+    && rm -rf mase
